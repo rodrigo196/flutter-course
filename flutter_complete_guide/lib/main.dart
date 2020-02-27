d0127636c15e580d8fb00b0bc8e221181bebfe3d
Rodrigo Fernandes Bulgarelli <eld_rfernandes@uolinc.com>
2020-02-27 16:40:03 -0300

    Functions to bottoms.

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,16 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-//void main(){
-// runApp(MyApp());
-//}
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+  void answerQuestion() {
+    print("Answer choosen!");
   }
 
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text("Answer 1"), onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Answer 2 choosen!"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"), onPressed: () {
+              print("Answer 3 choosen!");
+            },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
